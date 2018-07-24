68da4ab9d277c791e14409714441cfdc95de9c7f
Brian Abbott <briancabbott@gmail.com>
2018-07-25 01:26:24 +0200

    updated for uts

---
+++ test/ngram_calc_test.ts
@@ -0,0 +1,17 @@
+
+/** 
+ * Test sets to possibly find: 
+ *    https://rstudio-pubs-static.s3.amazonaws.com/96252_bd61a0777ad44d04b619ce95ca44219c.html
+ *    https://lagunita.stanford.edu/c4x/Engineering/CS-224N/asset/slp4.pdf
+ *    http://arno.uvt.nl/show.cgi?fid=127240 
+ *    https://en.wikipedia.org/wiki/N-gram
+ *    https://www2.lingfil.uu.se/SLTC2014/abstracts/sltc2014_submission_27.pdf
+ *    http://www.aclweb.org/anthology/C08-1038
+ *    http://citeseerx.ist.psu.edu/viewdoc/download?doi=10.1.1.187.3119&rep=rep1&type=pdf
+ */
+
+describe('', () => { 
+    it("", ()=> {
+
+    });
+}); 
